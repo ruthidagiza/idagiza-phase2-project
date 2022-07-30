b5941c491dd8dcf46be4cc4e85b6ecb6ab8f4588
IDAGIZA <ruthidagiza@gmail.com>
2022-07-30 20:05:10 +0300

    added countries page

---
+++ src/models/country_data.js
@@ -0,0 +1,11 @@
+class CountryData {
+  constructor(name, cases, deaths, lastupdate, fatalratio) {
+    this.name = name;
+    this.cases = cases;
+    this.deaths = deaths;
+    this.lastupdate = lastupdate;
+    this.fatalratio = fatalratio;
+  }
+}
+
+export default CountryData;

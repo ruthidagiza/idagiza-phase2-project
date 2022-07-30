1621bf7b7f7f568bdb417c5addc9b47c77dde84e
IDAGIZA <ruthidagiza@gmail.com>
2022-07-30 20:05:24 +0300

    added about us page

---
+++ src/pages/about_us.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AboutUsPage = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h5>We are a covid 19 tracking team</h5>
+    </div>
+  );
+};
+
+export default AboutUsPage;
